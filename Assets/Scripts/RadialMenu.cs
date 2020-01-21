2234b9205ebcea496f531583d9196cf2ba87f480
Igor Silchenko <kildsforkids@gmail.com>
2020-01-21 15:36:49 +1000

    Радиальное меню

---
+++ Assets/Scripts/RadialMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RadialMenu : MonoBehaviour
+{
+    [Header("Scene")]
+    public Transform selectionTransform;
+    public Transform cursorTransform;
+
+    [Header("Events")]
+    public RadialSection top;
+    public RadialSection right;
+    public RadialSection bottom;
+    public RadialSection left;
+}
---
+++ Assets/Scripts/RadialSection.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class RadialSection
+{
+    public Sprite icon;
+    public SpriteRenderer iconRenderer;
+    public UnityEvent onPress = new UnityEvent();
+}
