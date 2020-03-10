b54c5660fca8608eda315c7997d315fe9d4ade9d
FABLAB_VR_1 <kildsforkids@gmail.com>
2020-03-10 19:45:09 +1000

    Удалены объекты и добавлены точки телепортации

---
+++ Assets/Scripts/ShowControllers.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ShowControllers : MonoBehaviour
+{
+    [SerializeField]
+    private bool showControllers = false;
+
+    private void Update()
+    {
+        foreach (var hand in Player.instance.hands)
+        {
+            if (showControllers)
+            {
+                hand.ShowController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithController);
+            }
+            else
+            {
+                hand.HideController();
+                hand.SetSkeletonRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithoutController);
+            }
+        }
+    }
+}
