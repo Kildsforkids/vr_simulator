efb9c8386163e0ff077a2baa1fd74d642705b0b0
Igor Silchenko <kildsforkids@gmail.com>
2020-03-12 19:43:55 +1000

    Добавлены триггеры соприкосновения

--- Assets/Scenes/WheelPos.cs
+++ Assets/Scenes/WheelPos.cs
@@ -1,37 +1,41 @@
 ﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class WheelPos : MonoBehaviour
 {
-    public GameObject obj;
+    [SerializeField]
+    private GameObject obj;
+    private Quaternion startRotation = Quaternion.identity; 
 
-    void OnTriggerEnter(Collider obj)
+    private void OnTriggerEnter(Collider obj)
     {
-        Debug.Log("WORK");
-        transform.parent.position = new Vector3(transform.parent.position.x, 0.425f, -0.465f);
-        transform.parent.GetComponent<Rigidbody>().isKinematic = false;
-        transform.parent.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+        Debug.Log($"{this.obj.name} = {obj.name}");
+        if (this.obj.name == obj.name)
+        {
+            GetComponent<ThrowableExtend>().currentHand.DetachObject(gameObject);
+            GetComponent<Interactable>().enabled = false;
+            transform.rotation = startRotation;
+            transform.position = new Vector3(transform.position.x, 0.425f, -0.465f);
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation |
+                RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+
+    void OnTriggerStay(Collider obj)
+    {
+        if (this.obj.name == obj.name)
+        {
+        }
     }
 
     void OnTriggerExit(Collider obj)
     {
-        //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-        //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
-        //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
+        if (this.obj.name == obj.name)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation |
+                RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+        }
     }
 }
-
-//public class NutsPos : MonoBehaviour
-//{
-//    public GameObject rhoads;
-//    GameObject[] nuts = new GameObject[8];
-
-//    void OnTrigerEnter(Collider rhoads)
-//    {
-//        for (int i = 0; i < 8; i++)
-//        {
-//            nuts[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-//            nuts[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
-//            nuts[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
-//        }
-//    }
-//}
