b81bd768938e294cebc90d167cac068c0f4847ba
Igor Silchenko <kildsforkids@gmail.com>
2020-03-23 14:24:16 +1000

    Возврат объекта по нажатию кнопки
    Дополнительно включен центральный источник света, цвет фар заменен на более тусклый, отключены канвасы для точек телепортации, сохранены изменения префаба workshop

---
+++ Assets/Scripts/StartPosOpjRes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class StartPosOpjRes : MonoBehaviour
+{
+    private Vector3 StartPos;
+    private Quaternion StartRot;
+    private void Start()
+    {
+        StartPos = transform.position;
+        StartRot = transform.rotation;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = StartPos;
+            transform.rotation = StartRot;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+}
