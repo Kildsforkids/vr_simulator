e85ffe3b519270a6815d62bddc4ecc2aa77f0df5
Igor Silchenko <kildsforkids@gmail.com>
2020-03-26 18:13:04 +1000

    Работа подъемника и перемещение по трекпаду

---
+++ Assets/Scripts/Stream VR Additional/TrackpadMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class TrackpadMovement : MonoBehaviour
+{
+    [SerializeField]
+    private SteamVR_Action_Vector2 input;
+    [SerializeField]
+    private float speed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (input.axis.magnitude > 0.1f)
+        {
+            var direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+            transform.position += Vector3.ProjectOnPlane(direction, Vector3.up) * speed * Time.deltaTime;
+        }
+    }
+}
